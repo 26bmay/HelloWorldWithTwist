99a1ed886974a157bf6d922c3c8e0ec7117d1695
Bailey May <bsm95565@hawkmail.hacc.edu>
2025-09-08 19:16:28 -0400

    HelloTwist Files

---
+++ HelloTwist/HelloTwist/Program.cs
@@ -0,0 +1,12 @@
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, I'm AL. What is ur name?");
+        string name = Console.ReadLine();
+        Console.WriteLine($"{name}? What a lovely name! Tell me, what is your favorite hobby?");
+        string hobby = Console.ReadLine();
+        Console.WriteLine($"{hobby}? sounds super duper fun! Did you know that I was made by Bailey May?");
+        Console.WriteLine("Bailey May is a very talented cartoonist and writer. You should follow them on Instagram at @werewolfmay");
+    }
+}
